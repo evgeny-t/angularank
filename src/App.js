34a29599522cc250f6a29d0496b55e2547aa5589
etimoshenko <timoshenko.evgeny@gmail.com>
2017-06-10 23:01:50 +0200

    add virtualized collection of users

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Route, Link } from 'react-router-dom';
+
+// import FontIcon from 'material-ui/FontIcon';
+import SvgIcon from 'material-ui/SvgIcon';
+
 import AppBar from 'material-ui/AppBar';
 import Avatar from 'material-ui/Avatar';
 import Paper from 'material-ui/Paper';
@@ -15,7 +19,26 @@ import Popover from 'material-ui/Popover';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
 
+import CodeIcon from 'material-ui/svg-icons/action/code';
+import PeopleIcon from 'material-ui/svg-icons/social/people';
+
 import './App.css';
+
+import Measure from 'react-measure';
+
+import { Collection, WindowScroller } from 'react-virtualized';
+import 'react-virtualized/styles.css';
+
+// stolen from GitHub
+const RepoIcon = props => (
+  <SvgIcon role="img" viewBox="0 0 12 16"
+    {...props}
+  >
+    <path fillRule="evenodd" 
+      d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z">
+    </path>
+  </SvgIcon>
+);
 
 const UserDetails = ({ user }) => (
   <Paper zDepth={1}>
@@ -35,26 +58,98 @@ const UserDetailsContainer = connect(
   </div>
 ));
 
-const User = ({ user }) => (
-  <div style={{
-    height: 100,
-    width: 100,
-    display: 'inline-block',
-    border: '1px solid #ee1',
-  }}>
-    <img width={50} height={50} 
-      src={user.avatar_url} 
-      style={{ 
-        display: 'block',
-        margin: '0 auto',
-      }}
-    />
+const User = ({ user, style }) => (
+  <Paper 
+    zDepth={1}
+    style={{
+      ...style,
+      display: 'inline-block',
+      // border: '1px solid #ee1',
+    }}
+  >
+    <div style={{
+      position: 'relative',
+      height: 'calc(50% + 8px)',
+      border: '1px solid red',
+    }}>
+      <Avatar 
+        src={user.avatar_url} 
+        size={75}
+        style={{
+          display: 'block',
+          margin: 4,
+          position: 'absolute',
+        }}
+      />
+
+      <div style={{
+        width: '50%',
+        position: 'absolute',
+        left: '50%',
+      }}>
+
+        <div style={{
+          border: '1px solid #2ee',
+          display: 'inline-block',
+        }}>
+          <CodeIcon 
+            style={{
+              verticalAlign: 'middle',
+              border: '1px solid #ee2',
+              display: 'inline',
+            }}
+          />
+          <span style={{
+            verticalAlign: 'middle',
+            border: '1px solid #e2e',
+            fontSize: '0.8rem',
+          }}>{user.stat.total}</span>
+        </div>
+
+        <div style={{
+          border: '1px solid #2ee',
+          display: 'inline-block',
+        }}>
+          <PeopleIcon 
+            style={{
+              verticalAlign: 'middle',
+              border: '1px solid #ee2',
+              display: 'inline',
+            }}
+          />
+          <span style={{
+            verticalAlign: 'middle',
+            border: '1px solid #e2e',
+            fontSize: '0.8rem',
+          }}>{user.stat.total}</span>
+        </div>
+
+        <div style={{
+          border: '1px solid #2ee',
+          display: 'inline-block',
+        }}>
+          <RepoIcon
+            style={{
+              verticalAlign: 'middle',
+              border: '1px solid #ee2',
+              display: 'inline',
+            }}
+          />
+          <span style={{
+            verticalAlign: 'middle',
+            border: '1px solid #e2e',
+            fontSize: '0.8rem',
+          }}>{user.stat.total}</span>
+        </div>
+
+      </div>
+
+    </div>
+    
     <Link to={`/users/${user.id}`}>
       {user.login}
     </Link>
-    
-    <div>{user.stat.total}</div>
-  </div>
+  </Paper>
 );
 
 class RankFilter extends Component {
@@ -112,7 +207,32 @@ class RankFilter extends Component {
   }
 }
 
-const Users = connect(
+const UsersPageContent = props => 
+  <WindowScroller>
+    {({ height, isScrolling, scrollTop }) => (
+      <Collection 
+        cellCount={props.users.length}
+        cellRenderer={({ index, key, style }) => 
+          <User user={props.users[index]} 
+            key={key} style={style} />}
+        cellSizeAndPositionGetter={({ index }) => ({
+          height: 150,
+          width: 150,
+          x: (index % 5) * 150,
+          y: Math.trunc(index / 5) * 150,
+        })}
+        height={height}
+        width={150*5}
+
+        isScrolling={isScrolling}
+        scrollTop={scrollTop}
+        autoHeight
+        {...props}
+      />
+    )}
+  </WindowScroller>
+
+const UsersPage = connect(
   state => ({ 
     users: _.chain(state.users)
       .map(user => ({
@@ -129,27 +249,32 @@ const Users = connect(
   }),
   dispatch => ({}),
 )(({ users }) => {
-  return <div>
-    <Toolbar>
-      <ToolbarGroup>
-        <RankFilter />
-      </ToolbarGroup>
-    </Toolbar>
-    {users ? users.map(user => <User key={user.id} user={user} />) : null}
-  </div>
+  return (
+    <div>
+      <Toolbar>
+        <ToolbarGroup>
+          <RankFilter />
+        </ToolbarGroup>
+      </Toolbar>
+      <UsersPageContent 
+        users={users} 
+        style={{
+          margin: '4px auto',
+        }}
+      />
+    </div>);
 });
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-      <AppBar 
-        title={<Link style={{ 
-          textDecoration: 'none',
-          color: 'white',
-        }} to='/'>Angularank</Link>}
-      />
-        <Route exact path='/' component={Users} />
+        <AppBar 
+          title={<Link style={{ 
+            color: 'white',
+          }} to='/'>Angularank</Link>}
+        />
+        <Route exact path='/' component={UsersPage} />
         <Route path={`/users/:userid`} component={UserDetailsContainer} />
       </div>
     );
