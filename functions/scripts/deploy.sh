e864cba44dd130db418d23a360b1a5bb9c5e549f
etimoshenko <timoshenko.evgeny@gmail.com>
2017-06-17 21:30:21 +0200

    add a cloud functions for querying cloud datastore

--- functions/scripts/deploy.sh
+++ functions/scripts/deploy.sh
@@ -1,13 +1,2 @@
-gcloud beta functions deploy getState --stage-bucket evgenyt-1233 --trigger-http
-
-gcloud beta functions deploy prepareStats \
-  --stage-bucket evgenyt-1233 --trigger-topic=prepareStats \
-  --timeout=9m --memory=1024MB
-
-gcloud beta functions deploy processRepo \
-  --stage-bucket evgenyt-1233 --trigger-topic=processRepo \
-  --timeout=9m --memory=1024MB
-
-gcloud beta functions deploy updateUser \
-  --stage-bucket evgenyt-1233 --trigger-topic=updateUser \
-  --timeout=9m --memory=1024MB
+gcloud beta functions deploy query \
+  --stage-bucket evgenyt-1233 --trigger-http
