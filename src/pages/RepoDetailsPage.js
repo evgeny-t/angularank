f1fba2519061057207d4da762f9b32b4fe551ec2
etimoshenko <timoshenko.evgeny@gmail.com>
2017-06-18 23:22:57 +0200

    use User component

--- src/pages/RepoDetailsPage.js
+++ src/pages/RepoDetailsPage.js
@@ -2,15 +2,74 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 
+import SvgIcon from 'material-ui/SvgIcon';
+import Paper from 'material-ui/Paper';
+
+import { User, userToProps } from '../User';
+
+const StarIcon = props => (
+  <SvgIcon role="img" viewBox="0 0 14 16"
+    {...props}
+  >
+    <path fillRule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74z"></path>
+  </SvgIcon>
+);
+
+const EyeIcon = props => (
+  <SvgIcon role="img" viewBox="0 0 16 16"
+    {...props}
+  >
+    <path fillRule="evenodd" d="M8.06 2C3 2 0 8 0 8s3 6 8.06 6C13 14 16 8 16 8s-3-6-7.94-6zM8 12c-2.2 0-4-1.78-4-4 0-2.2 1.8-4 4-4 2.22 0 4 1.8 4 4 0 2.22-1.78 4-4 4zm2-4c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path>
+  </SvgIcon>
+);
+
+const ForkIcon = props => (
+  <SvgIcon role="img" viewBox="0 0 10 16"
+    {...props}
+  >
+    <path fillRule="evenodd" d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path>
+  </SvgIcon>
+);
+
+const IssueIcon = props => (
+  <SvgIcon role="img" viewBox="0 0 14 16"
+    {...props}
+  >
+    <path fillRule="evenodd" d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"></path>
+  </SvgIcon>
+);
+
 const RepoDetails = ({ repo, users }) => (
-  <div>
-    <div>{repo.full_name}</div>
-    <div>forks_count: {repo.forks_count}</div>
-    <div>open_issues_count: {repo.open_issues_count}</div>
-    <div>stargazers_count: {repo.stargazers_count}</div>
-    <div>watchers_count: {repo.watchers_count}</div>
-    {users.map(user => <div>{user.login}</div>)}
-  </div>
+  <Paper
+    zDepth={0}
+    style={{
+      marginLeft: '10%',
+      marginRight: '10%',
+    }}
+  >
+    <Paper
+      zDepth={1}
+      style={{padding:8, margin:8,}}
+    >
+      <h2>{repo.name}</h2>
+      <span><ForkIcon /> Forks {repo.forks_count}</span>
+      <span><IssueIcon /> Issues {repo.open_issues_count}</span>
+      <span><StarIcon /> Stargazers {repo.stargazers_count}</span>
+      <span><EyeIcon /> Watchers {repo.watchers_count}</span>
+    </Paper>
+    
+    
+    {users.map(user => (
+      <User user={user} 
+        key={user.id}
+        style={{
+          width: 150,
+          height: 110,
+          margin: 10,
+        }}
+      />
+    ))}
+  </Paper>
 );
 
 export const RepoDetailsPage = connect(
@@ -20,8 +79,10 @@ export const RepoDetailsPage = connect(
     <div>
       <RepoDetails 
         repo={repos[match.params.repoid]} 
-        users={_.map(repoToStats[match.params.repoid], 
-          (value, userid) => users[userid])}
+        users={_.chain(repoToStats[match.params.repoid])
+            .map((value, userid) => userToProps(users[userid]))
+            .sortBy(user => -user.stat.total)
+            .value()}
       />
     </div>
   )
--- src/pages/UsersPage.js
+++ src/pages/UsersPage.js
@@ -16,118 +16,13 @@ import Popover from 'material-ui/Popover';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
 
-import CodeIcon from 'material-ui/svg-icons/action/code';
-import PeopleIcon from 'material-ui/svg-icons/social/people';
-
 import Measure from 'react-measure';
 
 import { Collection, WindowScroller } from 'react-virtualized';
 import 'react-virtualized/styles.css';
 
-// stolen from GitHub
-const RepoIcon = props => (
-  <SvgIcon role="img" viewBox="0 0 12 16"
-    {...props}
-  >
-    <path fillRule="evenodd" 
-      d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z">
-    </path>
-  </SvgIcon>
-);
+import { User, userToProps, } from '../User';
 
-const styles = {
-  user: {
-    metricsContainer: {
-      position: 'absolute',
-      textAlign: 'left',
-      margin: 4,
-      right: 0,
-      width: `calc(100% - ${75 + 8 + 8}px)`,
-    },
-    metrics: {
-      display: 'block',
-      marginTop: 2,
-    },
-    icon: {
-      width: 15,
-      height: 15,
-      verticalAlign: 'middle',
-      display: 'inline',
-    },
-    value: {
-      verticalAlign: 'middle',
-      fontSize: '0.7rem',
-      margin: 2,
-    }
-  }
-}
-
-const User = ({ user, style }) => (
-  <Paper 
-    zDepth={1}
-    style={{
-      ...style,
-      display: 'inline-block',
-    }}
-  >
-    <div style={{
-      position: 'relative',
-      height: `${75 + 8}px`,
-    }}>
-      <Avatar 
-        src={user.avatar_url} 
-        size={75}
-        style={{
-          display: 'block',
-          margin: 4,
-          position: 'absolute',
-        }}
-      />
-
-      <div style={styles.user.metricsContainer}>
-
-        <div style={styles.user.metrics}>
-          <CodeIcon 
-            style={styles.user.icon}
-          />
-          <span style={styles.user.value}>
-            {user.stat.total}
-          </span>
-        </div>
-
-        <div style={styles.user.metrics}>
-          <PeopleIcon 
-            style={styles.user.icon}
-          />
-          <span style={styles.user.value}>
-            {user.stat.followers}
-          </span>
-        </div>
-
-        <div style={styles.user.metrics}>
-          <RepoIcon
-            style={styles.user.icon}
-          />
-          <span style={styles.user.value}>
-            {user.stat.repos}
-          </span>
-        </div>
-
-      </div>
-
-    </div>
-    
-    <Link 
-      to={`/users/${user.id}`}
-      style={{
-        display: 'block',
-        textAlign: 'center',
-      }}
-    >
-      {user.login}
-    </Link>
-  </Paper>
-);
 
 class RankFilter extends Component {
   state = {
@@ -223,20 +118,7 @@ export const UsersPage = connect(
   state => ({ 
     loading: state.usersLoading,
     users: _.chain(state.users)
-      .map(user => ({
-        ...user,
-        stat: _.chain(user.repos)
-          .reduce((acc, repoStats, repoId) => ({ 
-            ...acc,
-            total: acc.total + repoStats.total 
-          }), { 
-            total: 0,
-            repos: user.userToRepos,
-            followers: user.userToFollowers,
-          })
-          .value(),
-        
-      }))
+      .map(userToProps)
       .sortBy(user => -user.stat.total)
       .value()
   }),
